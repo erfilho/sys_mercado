ccc0f35f7d2cc0ef826e893d7608f8bfef074e62
erfilho <erineldx@gmail.com>
2023-10-23 20:04:47 -0300

    #07.2 - Adicionado os estados na seleção

--- routes/clientsRouter.js
+++ routes/clientsRouter.js
@@ -1,10 +1,40 @@
 const express = require("express");
 const router = express.Router();
 
+const estados = [
+  { uf: "AC", estado: "Acre" },
+  { uf: "AL", estado: "Alagoas" },
+  { uf: "AP", estado: "Amapá" },
+  { uf: "AM", estado: "Amazonas" },
+  { uf: "BA", estado: "Bahia" },
+  { uf: "CE", estado: "Ceará" },
+  { uf: "DF", estado: "Distrito Federal" },
+  { uf: "ES", estado: "Espírito Santo" },
+  { uf: "GO", estado: "Goiás" },
+  { uf: "MA", estado: "Maranhão" },
+  { uf: "MT", estado: "Mato Grosso" },
+  { uf: "MS", estado: "Mato Grosso do Sul" },
+  { uf: "MG", estado: "Minas Gerais" },
+  { uf: "PA", estado: "Pará" },
+  { uf: "PB", estado: "Paraíba" },
+  { uf: "PR", estado: "Paraná" },
+  { uf: "PE", estado: "Pernambuco" },
+  { uf: "PI", estado: "Piauí" },
+  { uf: "RJ", estado: "Rio de Janeiro" },
+  { uf: "RN", estado: "Rio Grande do Norte" },
+  { uf: "RS", estado: "Rio Grande do Sul" },
+  { uf: "RO", estado: "Rondônia" },
+  { uf: "RR", estado: "Roraima" },
+  { uf: "SC", estado: "Santa Catarina" },
+  { uf: "SP", estado: "São Paulo" },
+  { uf: "SE", estado: "Sergipe" },
+  { uf: "TO", estado: "Tocantins" },
+];
+
 // CONFIGURANDO A AUTENTICAÇÃO DE USUÁRIO
 router.get("/cadastro", (req, res) => {
   auth = true;
-  res.render("cadastro_cliente", { auth });
+  res.render("cadastro_cliente", { auth, estados });
 });
 
 // CONFIGURANDO A AUTENTICAÇÃO DE USUÁRIO
