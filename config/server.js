765a5bf2a617a118c46bcb5a38185ea176f76c1d
Erineldo Filho <erineldot@gmail.com>
2023-10-23 11:08:00 -0300

    #02 - Changed some archives

--- config/server.js
+++ config/server.js
@@ -1,83 +1,45 @@
-let express = require('express')
-let app = express()
-let consign = require('consign')
-let bodyParser = require('body-parser')
-let jsonParser = bodyParser.json()
-const { isReadable } = require('stream')
+let express = require("express");
+let app = express();
+let consign = require("consign");
+const exphbs = require("express-handlebars");
+const User = require("../routes/userRouter");
 
-// CONFIGURANDO O ENGINE DO EJS
-app.set('view engine', 'ejs')
-// CONFIGURANDO A PASTA DAS VIEWS
-app.set('views', './views')
-
-app.use(bodyParser.urlencoded({extended: true}))
-
-app.use(jsonParser)
-
-// CONFIGURANDO A ROTA DE LER QRCODE
-app.get('/read', (req, res) => {
-    let connection = app.config.dbConnection()
-    //aula 42
-    let qrCode = new app.models.qrcodeDAO(connection)
-    //aula 42
-    qrCode.getQrcode((err, rest) => {
-        res.render("qrcode/ler", {qrcode: rest})
-    })
-})
-
-app.get('/qrcode/save', (req, res) => {
-    res.render("qrcode/ler");
-})
-
-app.post('/qrcode/save', jsonParser,(req, res) => {
-    // Lendo a requisição
-    let qrcode = req.body;
-    // Abrindo a conexão
-    let connection = app.config.dbConnection()
-    // Criando o model do qrCode
-    let qrCode = new app.models.qrcodeDAO(connection)
-    // Salvando no banco de dados
-    qrCode.salvarQrcode(qrcode, (err, result) => {
-        console.log(err)
-    })
-})
-
-app.post('/qrcode/check', jsonParser, (req, res) => {
-    // Lendo a requisição
-    let valor = req.body;
-    // Abrindo a conexão
-    let connection = app.config.dbConnection()
-    // Criando o model do qrcode
-    let qrCode = new app.models.qrcodeDAO(connection)
-    // Verificando no banco de dados
-    qrCode.getQrcode((err, result) => {
-        // Caso ocorra algum erro
-        if(err){ 
-            res.render('qrcode/erro', {err: erro})
-        }
-        // Lendo o valor do último qrcode que foi gravado
-        ultimo_val = result[result.length-1].valor
-        // Se o valor lido for igual ao último valor do qr code irá ser marcada a sua presença
-        if(valor.valor == ultimo_val) {
-            res.render('qrcode/presencamarcada')
-        } else {
-            // Caso não seja a página vai ser recarregada
-            res.redirect('/read')
-        }
-    })
-})
-
-// CONFIGURANDO A ROTA DO INDEX
-app.get('/', (req, res) => {
-    res.render('home/index')
-})
-
+// CONFIGURANDO O HANDLEBARS
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+const hbs = exphbs.create({
+  partialsDir: ["views/partials"],
+});
 
 // CONFIGURANDO A PASTA DOS ARQUIVOS ESTÁTICOS
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-consign().then('/config/dbConnection.js')
-         .then('/models')
-        .into(app)
+// CONFIGURANDO O PARSER
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
 
-module.exports = app
+// CONFIGURANDO O USO DAS ROTAS
+app.use("/users", User);
+
+// CONFIGURANDO AS ROTAS
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+// CONFIGURANDO A ROTA DE LOGIN
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+// CONFIGURANDO O 404
+app.use(function (req, res) {
+  res.status(404).render("404");
+});
+
+consign().then("/config/dbConnection.js").then("/models").into(app);
+
+module.exports = app;
---
+++ routes/userRouter.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+
+var auth = false;
+const usuario = [
+  {
+    login: "admin",
+    senha: "admin",
+  },
+];
+
+// CONFIGURANDO A AUTENTICAÇÃO DE USUÁRIO
+router.post("/login", (req, res) => {
+  const user = req.body.user;
+  const password = req.body.password;
+  console.log(user, password);
+  let message = "";
+  if (usuario[0].login === user && usuario[0].senha === password) {
+    auth = true;
+    message = "Login efetuado com sucesso!";
+    res.render("pagina_principal", { usuario, auth, message });
+  } else {
+    auth = false;
+    message = "Usuário e/ou senha inválidos!";
+    res.render("login", { auth, message });
+  }
+});
+
+//CONFIGURANDO LOGOUT
+router.get("/logout", (req, res) => {
+  auth = false;
+  res.render("login", { auth });
+});
+
+module.exports = router;
