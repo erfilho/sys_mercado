1011994d9805f9b2e81daec566c43b1538ec7ff5
AlanMarinho <a13br1313@gmail.com>
2023-10-31 11:23:14 -0300

    #28 - Correção - estado do cliente não era salvo em banco

--- public/js/read_script.js
+++
@@ -1,9 +0,0 @@
-function onScanSuccess(decodedText, decodedResult) {
-    console.log(`Code scanned = ${decodedText}`, decodedResult);
-    document.getElementById("result").innerText = decodedText;
-    document.getElementById("result-post").value = decodedText;
-    document.getElementById("form-post").submit()
-}
-var html5QrcodeScanner = new Html5QrcodeScanner(
- "qr-reader", { fps: 10, qrbox: 250 });
-html5QrcodeScanner.render(onScanSuccess);
